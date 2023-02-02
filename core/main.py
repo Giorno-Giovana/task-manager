6c50287b94f4e5637aa8f794f65c98bad9d3d92e
ded <89196994468@yandex.ru>
2023-02-02 19:41:54 +0300

    пишу время в файл test.txt

--- core/main.py
+++ core/main.py
@@ -3,5 +3,7 @@ import os
 
 if __name__ == "__main__":
     # print(datetime.datetime.now())
-    with open(os.path.dirname(os.path.abspath(__file__))+'/data/test.txt', 'a', encoding='utf-8') as file:
+    with open('test.txt', 'a', encoding='utf-8') as file:
         file.write(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '\n')
+
+        # os.path.dirname(os.path.abspath(__file__)) +
