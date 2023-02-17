00de0edde6f8bae7d7ebb4dda65554e913362862
ded <89196994468@yandex.ru>
2023-02-17 20:34:21 +0300

    пишу время в файл test.txt с помощью python

---
+++ core/cron.py
@@ -0,0 +1,7 @@
+from core.tests_old import time
+
+if __name__ == "__main__":
+    while 1>0:
+        # Запускаем файл 'main.py' каждую минуту
+        exec(open('main.py','r').read())
+        time.sleep(60)
