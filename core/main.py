b153b2ef4220c94bd1510cca83033fbe3a2272ad
ded-m <117845355+ded-m@users.noreply.github.com>
2023-03-01 18:14:24 +0300

    Delete main.py

--- core/main.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-if __name__ == "__main__":
-    with open('test.txt', 'a', encoding='utf-8') as file:
-        file.write(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '\n')
